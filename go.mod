ac561e45d0f7f791b8decabae5abf4ce9e53ffb9
hollinsStuart <hollinsyu@gmail.com>
2025-07-01 19:39:21 +0800

    feat: basic functions finished

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/hollinsStuart/lsgo
+
+go 1.23.5
+
+require (
+	github.com/fatih/color v1.18.0
+	github.com/olekukonko/tablewriter v1.0.7
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/errors v0.0.0-20250405072817-4e6d85265da6 // indirect
+	github.com/olekukonko/ll v0.0.8 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/renderer"
+	"github.com/olekukonko/tablewriter/tw"
+	"os"
+	"path/filepath"
+)
+
+type EntryType string
+
+const (
+	File EntryType = "File"
+	Dir  EntryType = "Dir"
+)
+
+type FileEntry struct {
+	Name     string    `json:"name"`
+	EType    EntryType `json:"type"`
+	LenBytes int64     `json:"bytes"`
+	Modified string    `json:"modified"`
+}
+
+func getFiles(path string) ([]FileEntry, error) {
+	var data []FileEntry
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			continue
+		}
+		eType := File
+		if entry.IsDir() {
+			eType = Dir
+		}
+		modified := info.ModTime().Format("Mon Jan 2 2006")
+		data = append(data, FileEntry{
+			Name:     entry.Name(),
+			EType:    eType,
+			LenBytes: info.Size(),
+			Modified: modified,
+		})
+	}
+	return data, nil
+}
+
+func printTable(files []FileEntry) {
+	data := make([][]string, len(files))
+	for i, f := range files {
+		data[i] = []string{
+			f.Name,
+			string(f.EType),
+			fmt.Sprintf("%d", f.LenBytes),
+			f.Modified,
+		}
+	}
+
+	colorCfg := renderer.ColorizedConfig{
+		Header: renderer.Tint{
+			FG: renderer.Colors{color.FgGreen, color.Bold},
+			BG: renderer.Colors{color.BgHiWhite},
+		},
+		Column: renderer.Tint{
+			FG: renderer.Colors{color.FgCyan}, // default
+			Columns: []renderer.Tint{
+				{FG: renderer.Colors{color.FgMagenta}},  // Name
+				{},                                      // Type
+				{FG: renderer.Colors{color.FgHiYellow}}, // Bytes
+				{FG: renderer.Colors{color.FgHiRed}},    // Modified
+			},
+		},
+		Footer: renderer.Tint{
+			FG: renderer.Colors{color.FgYellow, color.Bold},
+		},
+		Border:    renderer.Tint{FG: renderer.Colors{color.FgWhite}},
+		Separator: renderer.Tint{FG: renderer.Colors{color.FgWhite}},
+	}
+
+	table := tablewriter.NewTable(os.Stdout,
+		tablewriter.WithRenderer(renderer.NewColorized(colorCfg)),
+		tablewriter.WithConfig(tablewriter.Config{
+			Row: tw.CellConfig{
+				Formatting:   tw.CellFormatting{AutoWrap: tw.WrapNormal},
+				Alignment:    tw.CellAlignment{Global: tw.AlignLeft},
+				ColMaxWidths: tw.CellWidth{Global: 25},
+			},
+			Footer: tw.CellConfig{
+				Alignment: tw.CellAlignment{Global: tw.AlignRight},
+			},
+		}),
+	)
+
+	table.Header([]string{"Name", "Type", "Bytes", "Modified"})
+	table.Bulk(data)
+	table.Footer([]string{"", "Total Files", fmt.Sprintf("%d", len(files)), ""})
+	table.Render()
+}
+
+func main() {
+	var jsonOutput bool
+	flag.BoolVar(&jsonOutput, "json", false, "output in JSON format")
+	flag.Parse()
+
+	path := "."
+	if flag.NArg() > 0 {
+		path = flag.Arg(0)
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		color.Red("Error resolving path: %v", err)
+		return
+	}
+
+	if _, err := os.Stat(absPath); os.IsNotExist(err) {
+		color.Red("Path does not exist!")
+		return
+	}
+
+	files, err := getFiles(absPath)
+	if err != nil {
+		color.Red("Error reading directory: %v", err)
+		return
+	}
+
+	if jsonOutput {
+		jsonData, err := json.MarshalIndent(files, "", "  ")
+		if err != nil {
+			color.Red("JSON serialization error: %v", err)
+			return
+		}
+		fmt.Println(string(jsonData))
+	} else {
+		printTable(files)
+	}
+}
